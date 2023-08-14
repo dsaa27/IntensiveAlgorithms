3baadbe0318a38b6afaa6c52f3b421e601bb3a7c
a.dolmatov <aleksandr.dolmatov2012@gmail.com>
2023-08-14 23:21:12 +0500

    simple brackets solution

--- Brackets/brackets.hpp
+++ Brackets/brackets.hpp
@@ -5,5 +5,17 @@
 // https://leetcode.com/problems/valid-parentheses/
 class Solution {
 public:
-  bool isValid(std::string s) {}
+    bool isValid(string s) {
+        std::vector<char> brackets;
+        for (const auto ch : s) {
+            if (ch == ')') {
+                if (brackets.empty())
+                    return false;
+                brackets.pop_back();
+            } else {
+                brackets.push_back(' ');
+            }
+        }
+        return brackets.empty();
+    }
 };
